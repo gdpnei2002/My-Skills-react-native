4d12e6a8c1196a13940f7a8ca0983861a42f8d20
neilson <joseneilson25@gmail.com>
2022-07-24 23:45:20 -0300

    Tipando componentes com TypeScript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,7 +49,10 @@ export function Home(){
             onChangeText={setNewSkill}
             />
 
-            <Button onPress={handleAddNewSkill} />
+            <Button 
+                title="Add"
+                onPress={handleAddNewSkill}
+                 />
 
             <Text style={[styles.title, {marginVertical: 50}]}>
                 My skills
